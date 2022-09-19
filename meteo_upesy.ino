4f29b053e1138ab1bae4dba6cd640bb8f4ceadb9
batnoe <96292219+batnoe@users.noreply.github.com>
2022-09-19 13:34:43 +0200

    Update meteo_upesy.ino

--- meteo_upesy.ino
+++ meteo_upesy.ino
@@ -4,20 +4,19 @@
 #include <WiFi.h>
 #include "SPI.h"
 #include "TFT_eSPI.h"
-// station météo upesy tft 3.5 bme280
 
 #include <BME280I2C.h> 
 #include <Wire.h>
 #include <time.h>
-const char* ssid       = "DNA-Mokkula-2G-7M3EQF";
+/*const char* ssid       = "DNA-Mokkula-2G-7M3EQF";
 const char* password   = "47890783266";
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
-const int   daylightOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;*/
 
 float temp_ext = 0;   float t_max = temp_ext;   float t_min = 30;
-float humidite;
-long temps;
+float humidite; float temp_moy;  int nb;
+long temps; long temps_moy;
 
 typedef struct struct_message {
     float c;
@@ -33,7 +32,9 @@ TFT_eSPI myGLCD = TFT_eSPI();       // Invoke custom library
 
 void setup()                         // ----- Début du setup ----------------
 { 
-  Serial.printf("Connecting to %s ", ssid);
+  Serial.begin(115200);
+
+  /*Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -47,9 +48,8 @@ void setup()                         // ----- Début du setup ----------------
 
   //disconnect WiFi as it's no longer needed
   WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_OFF);*/
 
-  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   while(!Serial) {} // Wait
   Wire.begin();
@@ -86,10 +86,15 @@ void setup()                         // ----- Début du setup ----------------
   myGLCD.drawString("TEMP IN", 10, 100,4);
   myGLCD.drawString("PRESSION", 10, 180,4);
   myGLCD.drawString("HUMIDITE", 10, 260,4);
+  //myGLCD.setTextDatum(BC_DATUM); // Centre text on x,y position
+  //myGLCD.drawString("Moyenne du jour", 120, 470,2);
+  //myGLCD.setTextDatum(TL_DATUM); // Remet text a default
   myGLCD.setTextDatum(BC_DATUM); // Centre text on x,y position
-  myGLCD.drawString("Bernard.picasa14@gmail.com", 160, 470,2);
+  myGLCD.setTextColor(TFT_WHITE,TFT_BLACK); 
+  myGLCD.drawString("STATION  METEO", 160, 50,4);
   myGLCD.setTextDatum(TL_DATUM); // Remet text a default
-  printLocalTime();
+  
+  //printLocalTime();
   temps = millis();
 }                                   // ---------------- Fin du setup ------------------
 
@@ -108,15 +113,19 @@ void loop()                        // --------------- Début de la loop --------
   myGLCD.drawString("PRESSION", 10, 180,4);
   myGLCD.drawString("HUMIDITE", 10, 260,4); 
   myGLCD.setTextDatum(BC_DATUM); // Centre text on x,y position
-  myGLCD.drawString("Bernard.picasa14@gmail.com", 160, 470,2);
+  //myGLCD.drawString("Moyenne du jour", 120, 470,2);
   myGLCD.setTextDatum(TL_DATUM); // Remet text a default 
+  myGLCD.setTextDatum(BC_DATUM); // Centre text on x,y position
+  myGLCD.setTextColor(TFT_WHITE,TFT_BLACK);
+  myGLCD.drawString("STATION  METEO", 160, 50,4);
+  myGLCD.setTextDatum(TL_DATUM); // Remet text a default
 
   myGLCD.setTextColor(TFT_GREEN,TFT_BLACK);
-  myGLCD.drawFloat(temp + 0.5, 1, 210, 90, 6);         //temp_in -3.7 TFT 2.8
+  myGLCD.drawFloat(temp-0.5 , 1, 210, 90, 6);         //temp_in -3.7 TFT 2.8
   myGLCD.drawNumber(pres/100+18, 200, 170, 6);
   myGLCD.drawNumber(hum + 4, 250, 250, 6);
   
-  printLocalTime();
+  //printLocalTime();
   temps = millis() ;}       //  delay (1000*60);
 }                               
 // --------------- Fin de la loop -----------------
@@ -126,15 +135,24 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {    
   temp_ext = myData.c;
   humidite = myData.d;
    if (temp_ext > t_max) {t_max = temp_ext;} else if(temp_ext < t_min and t_min > -30 and temp_ext > -50) {t_min = temp_ext;}    // -------- calcul mini et maxi température extérieur ---------------
-  myGLCD.setTextColor(TFT_BLUE,TFT_BLACK);
-  myGLCD.drawNumber(humidite, 160, 250, 6);
+  myGLCD.setTextColor(TFT_YELLOW,TFT_BLACK);
+  myGLCD.drawNumber(humidite-2, 160, 250, 6);
   //if (temp_ext < 10)  { myGLCD.drawFloat(temp_ext, 1, 220, 90, 6); } else { myGLCD.drawFloat(temp_ext, 1, 210, 90, 6); }  // affiche température extérieur
   //myGLCD.setTextColor(TFT_WHITE,TFT_BLACK); myGLCD.drawFloat(t_max, 1, 150, 80, 5); myGLCD.drawFloat(t_min, 1, 150, 110, 5);  //affiche mini maxi
 
   myGLCD.setTextColor(TFT_ORANGE,TFT_BLACK); myGLCD.drawFloat(temp_ext, 1, 130, 340, 8);
-  myGLCD.setTextColor(TFT_RED,TFT_BLACK); myGLCD.drawFloat(t_max, 1, 10, 330, 6); myGLCD.setTextColor(TFT_BLUE,TFT_BLACK); myGLCD.drawFloat(t_min, 1, 10, 400, 6);  //affiche mini maxi
+  myGLCD.setTextColor(TFT_YELLOW,TFT_BLACK); myGLCD.drawFloat(t_max, 1, 10, 330, 6); myGLCD.setTextColor(TFT_YELLOW,TFT_BLACK); myGLCD.drawFloat(t_min, 1, 10, 400, 6);  //affiche mini maxi
+
+/*if ( (millis() - temps_moy) > 1000*60) {
+      // affichage de la moyenne température exterieur
+      nb = ++nb;  temp_moy=temp_moy+temp_ext;  
+      temps_moy = millis();
+    }
+    myGLCD.setTextColor(TFT_PINK,TFT_BLACK);   myGLCD.drawFloat(temp_moy/nb, 1, 210, 445, 4);
+    */
 }
-void printLocalTime()
+
+/*void printLocalTime()
 {
   time_t rawtime;
   struct tm timeinfo;
@@ -151,4 +169,4 @@ void printLocalTime()
   //Optional: Construct String object 
   String asString(timeStringBuff);
   myGLCD.drawString(asString, 15, 15, 6);
-}
+}*/
